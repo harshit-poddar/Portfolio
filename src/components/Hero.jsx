ba94229de396efd9885e910cc1db3780207bdc59
Harshit Poddar <poddarharshit.2000@gmail.com>
2023-03-29 21:00:22 +0530

    updated without canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import {tempCanvas} from "./canvas";
 
 const Hero = () => {
   return (
@@ -25,7 +26,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <ComputersCanvas />
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
