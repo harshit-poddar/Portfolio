cf8da0727c1d2a63a90be275dc04acc6edc2ac95
Harshit Poddar <poddarharshit.2000@gmail.com>
2023-03-30 07:34:47 +0530

    deletion and addition in assets

---
+++ public/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.png";
+import backend from "./backend.gif";
+import creator from "./creator.gif";
+import mobile from "./mobile.gif";
+import web from "./web.gif";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import man from "./man.gif"
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import ml from "./tech/ml.png";
+import dl from "./tech/dl.png";
+import sql from "./tech/sql.png";
+import tf from "./tech/tf.png";
+import pytorch from "./tech/pytorch.png";
+import python from "./tech/python.png";
+
+
+
+import meta from "./company/meta.png";
+import purchase from "./company/purchase.gif";
+import lib from "./company/lib.gif";
+import data from './company/data.gif'
+import music from "./company/music.gif";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  purchase,
+  data,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  meta,
+  lib,
+  music,
+  carrent,
+  jobit,
+  tripguide,
+  ml,
+  dl,
+  sql,
+  python,
+  pytorch,
+  tf,
+  man
+};
